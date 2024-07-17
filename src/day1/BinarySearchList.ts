b801f83390d273fe4e0e399eb747c643128df0e0
Rajakumardev <rajakumarofficial@gmail.com>
2024-07-17 16:15:33 +0000

    binsearch

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,14 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let lo = 0;
+    let hi = haystack.length;
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        if (haystack[m] === needle) return true;
+        if (needle > haystack[m]) {
+            lo = m + 1;
+        } else if (needle < haystack[m]) {
+            hi = m;
+        }
+    } while (lo < hi);
+    return false;
 }
